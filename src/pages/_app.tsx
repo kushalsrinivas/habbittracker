63d37064cfd3fa88a002c1df394cec982366f048
kushalsrinivas <kushal.s.2005@gmail.com>
2024-04-21 04:11:34 +0530

    Aasl;kd

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,11 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { ActivityProvider } from "@/context/store";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <ActivityProvider>
+      <Component {...pageProps} />;
+    </ActivityProvider>
+  );
 }
