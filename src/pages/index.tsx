94bf544e167f5231fde31e8f60b7e1424d2b017a
kushalsrinivas <kushal.s.2005@gmail.com>
2024-04-21 09:39:08 +0530

    Adskass;lka;lkdas;kd

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -54,7 +54,7 @@ export default function Home() {
       <script
         async
         src="https://platform.twitter.com/widgets.js"
-        charset="utf-8"
+        charSet="utf-8"
       ></script>
     </>,
   ];
